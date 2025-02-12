756da7ebfecdfd0b2168ebcf36755ea2a2fbd9d9
Icaka_XD <hristodincov@gmail.com>
2025-02-12 22:34:52 +0100

    AnswerCheck and dialog speed added

---
+++ Assets/Scripts/AnswerChecker.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerChecker : MonoBehaviour
+{
+    [SerializeField] List<CardManager> cards;
+    [SerializeField] List<int> levelAnswerCard;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
